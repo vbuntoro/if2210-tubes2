7e7f7a0909372bc6601507814502f85d428e3ab8
Imballinst <lucent_rays@yahoo.com>
2014-05-19 23:10:12 +0700

    Mainmenu update

--- if2210-tubes2/src/tubes2/Mainmenu.java
+++ if2210-tubes2/src/tubes2/Mainmenu.java
@@ -4,6 +4,7 @@
 package tubes2;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -13,21 +14,51 @@ import java.awt.event.*;
  */
 class Mainmenu extends JFrame
 {
-	
+	private static final int BUTTON_LOCATION_X = 200;  // location x 
+	private static final int BUTTON_LOCATION_Y = 50;   // location y 
+	private static final int BUTTON_SIZE_X = 140;      // size height
+	private static final int BUTTON_SIZE_Y = 50;       // size width
 	/**
 	 * Instantiates a new mainmenu.
 	 */
 	public Mainmenu() {
-		setTitle("Rhythm-Pighting Game!");
-		setSize(400,400);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-		setLayout(new BorderLayout());
-		setContentPane(new JLabel(new ImageIcon("img/mainmenu.jpg")));
-		setLayout(new FlowLayout());
-		// Just for refresh :) Not optional!
-		setSize(800,600);
+		JFrame frame;
+		Container contentPane;
+		FlowLayout layout;
+
+		frame = new JFrame();
+		frame.setTitle("Rhythm-Pighting Game!");
+
+		ImageIcon icon = new ImageIcon("img/mainmenu.jpg");
+		JLabel label = new JLabel(icon);
+
+		/****************************/
+		frame.setContentPane(label);
+		contentPane = frame.getContentPane();
+		/****************************/
+
+		JButton startButton = new JButton("Click Me To Start!");
+        startButton.setBounds(BUTTON_LOCATION_X
+                            , BUTTON_LOCATION_Y,
+                              BUTTON_SIZE_X, 
+                              BUTTON_SIZE_Y );
+        //JFrame layout
+        this.setLayout(null);
+
+        //JPanel layout
+        startButton.setLayout(null);
+        contentPane.add(startButton);
+		
+		layout = new FlowLayout();
+		contentPane.setLayout(layout);
+
+		/****************************/
+		//contentPane.add(label);
+		/****************************/
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 	/**
