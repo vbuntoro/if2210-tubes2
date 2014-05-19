0debfa497ef4e9f1899b488f710ea37723f42d8d
Imballinst <lucent_rays@yahoo.com>
2014-05-19 16:47:22 +0700

    Map + Path

---
+++ if2210-tubes2/src/tubes2/Map.java
@@ -0,0 +1,81 @@
+package tubes2;
+
+public class Map {
+	/* Atribut kelas */
+	private int Level;
+	private Path[][] P;
+	/* Konstruktor */
+	public Map() {
+		/* Deklarasi array 2 dimensi */
+		P = new Path[8][];
+		for (int i=0;i<8;i++) {
+			P[i] = new Path[7];
+			for (int j=0;j<7;j++) {
+				P[i][j] = new Path();
+			}
+		}
+		/* Inisialisasi level 1 */
+		Level = 1;
+		randomMap();
+	}
+	/* Melakukan validasi, 1 adalah depan, 2 adalah belakang, 3 adalah kiri, 4 adalah kanan */
+	public boolean isPathValid(int arah, int i, int j) {
+		/* Melakukan validasi terhadap arah yang dipencet player terhadap map */
+		boolean x = false;
+		switch(arah) {
+			case 1: x = P[i][j].getFront(); break;
+			case 2: x = P[i][j].getBack(); break;
+			case 3: x = P[i][j].getLeft(); break;
+			case 4: x = P[i][j].getRight(); break;
+		}
+		return x;
+	}
+	public void increaseMapLevel() {
+		/* Menambahkan level map dan melakuka random map */
+		Level++;
+		randomMap();
+	}
+	public int getMapLevel() {
+		/* Mengembalikan level map */
+		return Level;
+	}
+	/* Randomizer map */
+	public void randomMap() {
+		/* Melakukan random map dengan 3 pattern
+		 * Setiap pattern map memiliki perbedaan arah dan goal
+		 */
+		/* clear bekas map sebelumnay */
+		P[0][5].setGoal(false); P[0][2].setGoal(false);
+		P[6][2].setStart(false); P[6][3].setStart(false);
+		/* random map */
+		int x = (int)(Math.random() * 2) + 1; //random 1-2
+		switch (x) {
+			case 1: P[0][3].setPath(8); P[0][5].setPath(4); P[0][5].setGoal(true);
+					P[1][3].setPath(4); P[1][4].setPath(8); P[1][5].setPath(4); P[1][6].setPath(8);
+					P[2][2].setPath(8); P[2][3].setPath(5); P[2][4].setPath(1); P[2][5].setPath(3); P[2][6].setPath(2);
+					P[3][1].setPath(8); P[3][2].setPath(8); P[3][4].setPath(4); P[3][6].setPath(4);
+					P[4][1].setPath(7); P[4][2].setPath(1); P[4][3].setPath(4); P[4][4].setPath(3); P[4][5].setPath(4);
+										P[4][6].setPath(2);
+					P[5][2].setPath(4); P[5][4].setPath(2); P[5][5].setPath(4); P[5][6].setPath(6);
+					P[6][2].setPath(4); P[6][2].setStart(true); P[6][4].setPath(8);
+					break;
+			case 2: P[0][1].setPath(8); P[0][2].setPath(4); P[0][2].setGoal(true); P[0][5].setPath(8); P[0][6].setPath(8);
+					P[1][1].setPath(4); P[1][2].setPath(4); P[1][3].setPath(8); P[1][4].setPath(7); P[1][5].setPath(3);
+										P[1][6].setPath(8);
+					P[2][0].setPath(8); P[2][1].setPath(1); P[2][2].setPath(3); P[2][3].setPath(3); P[2][4].setPath(4);
+										P[2][6].setPath(4);
+					P[3][1].setPath(4); P[3][3].setPath(4); P[3][4].setPath(8); P[3][6].setPath(4);
+					P[4][1].setPath(4); P[4][3].setPath(4); P[4][5].setPath(7); P[4][6].setPath(5);
+					P[5][0].setPath(8); P[5][1].setPath(5); P[5][2].setPath(4); P[5][3].setPath(1); P[5][4].setPath(4);
+										P[5][5].setPath(7); P[5][6].setPath(2);
+					P[6][0].setPath(7); P[6][1].setPath(7); P[6][3].setPath(4); P[6][3].setStart(true); P[6][6].setPath(8);
+					break;
+		}
+		
+	}
+	/* Main */
+	public static void main(String args[]) {
+		Map M = new Map();
+		M.randomMap();
+	}
+}
---
+++ if2210-tubes2/src/tubes2/Path.java
@@ -0,0 +1,66 @@
+package tubes2;
+
+public class Path {
+	/* Atribut kelas */
+	private boolean Front; //apabila ada jalan kedepan
+	private boolean Back; //apabila ada jalan kebelakang
+	private boolean Left; //apabila ada jalan kekiri
+	private boolean Right; //apabila ada jalan kekanan
+	private boolean Goal; //apabila merupakan goal
+	private boolean Start; //apabila merupakan start
+	private int Facing; //mengubah value facing yang merupakan sebuah permasalah pada FPS
+	/* Konstruktor dan Konstruktor parameter */
+	public Path() { 
+		Front = false;
+		Back = false;
+		Left = false;
+		Right = false;
+		Goal = false;
+		Start = false;
+	}
+	/* Setter */
+	public void setPath (int a) {
+		switch (a) {
+			case 1: Front = true; Back = true; Left = true; Right = true; break;
+			case 2: Front = true; Back = true; Left = true; Right = false; break;
+			case 3: Front = true; Back = true; Left = false; Right = true; break;
+			case 4: Front = true; Back = true; Left = false; Right = false; break;
+			case 5: Front = false; Back = true; Left = true; Right = true; break;
+			case 6: Front = false; Back = true; Left = true; Right = false; break;
+			case 7: Front = false; Back = true; Left = false; Right = true; break;
+			case 8: Front = false; Back = true; Left = false; Right = false; break;
+		}
+	}
+	public void setGoal(boolean a) {
+		Goal = a;
+	}
+	public void setStart(boolean a) {
+		Back = false;
+		Start = a;
+	}
+	public void setFacing(int a) {
+		Facing = a;
+	}
+	/* Getter */
+	public boolean getFront() {
+		return Front;
+	}
+	public boolean getBack() {
+		return Back;
+	}
+	public boolean getLeft() {
+		return Left;
+	}
+	public boolean getRight() {
+		return Right;
+	}
+	public boolean getGoal() {
+		return Goal;
+	}
+	public boolean getStart() {
+		return Start;
+	}
+	public int getFacing() {
+		return Facing;
+	}
+}
