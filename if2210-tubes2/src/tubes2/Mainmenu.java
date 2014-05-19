b9eb32a88646958eed0fd6c2244757c2b04da2b5
Imballinst <lucent_rays@yahoo.com>
2014-05-19 17:47:45 +0700

    Mainmenu update

--- if2210-tubes2/src/tubes2/Mainmenu.java
+++ if2210-tubes2/src/tubes2/Mainmenu.java
@@ -1,22 +1,31 @@
 package tubes2;
 
-import java.awt.*; 
-import java.awt.event.*; 
 import javax.swing.*;
-
-public class Mainmenu extends JFrame
+import java.awt.*;
+import java.awt.event.*;
+class Mainmenu extends JFrame
 {
-	  JPanel pane = new JPanel();
-	  JButton pressme = new JButton("Play the Game!");
-	  Mainmenu()        // the frame constructor
-	  {
-	    super("Rhythm-Pighting Game!"); setBounds(100,100,300,200);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    Container con = this.getContentPane(); // inherit main frame
-	    con.add(pane);    // JPanel containers default to FlowLayout
-	    pressme.setMnemonic('P'); // associate hotkey to button
-	    pane.add(pressme); pressme.requestFocus();
-	    setVisible(true); // make frame visible
-	  }
-	  public static void main(String args[]) {new Mainmenu();}
+	JButton b1;
+	JLabel l1;
+	public Mainmenu()
+	{
+		setTitle("Rhythm-Pighting Game!");
+		setSize(400,400);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setLayout(new BorderLayout());
+		setContentPane(new JLabel(new ImageIcon("img/mainmenu.jpg")));
+		setLayout(new FlowLayout());
+		l1=new JLabel("Here is a button");
+		b1=new JButton("I am a button");
+		add(l1);
+		add(b1);
+		// Just for refresh :) Not optional!
+		setSize(596,380);
+		}
+		public static void main(String args[])
+		{
+		new Mainmenu();
+	}
 }
